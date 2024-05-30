7a90da8d9706ac3fb70723d01d8311d33b71527c
a1nkitt <ankit131raj@gmail.com>
2024-05-30 19:53:37 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,9 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
-  // deleteUserFailure,
-  // deleteUserStart,
-  // deleteUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
   // signOutUserStart,
 } from '../redux/user/userSlice';
 
@@ -94,6 +94,22 @@ const dispatch = useDispatch();
       dispatch(updateUserFailure(error.message));
     }
   };
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -158,7 +174,7 @@ const dispatch = useDispatch();
         </form>
         <div className='flex justify-between mt-5'>
         <span
-          //onClick={handleDeleteUser}
+          onClick={handleDeleteUser}
           className='text-red-700 cursor-pointer'
         >
           Delete account
